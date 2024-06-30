2bcc761c7a55ca0d96673c9903f8487d4a1fffc6
Declimator <kyleyinxu@gmail.com>
2024-06-30 01:15:35 -0700

    Initial Commit

---
+++ src/view.hpp
@@ -0,0 +1,31 @@
+#ifndef VIEW_HPP
+#define VIEW_HPP
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <math.h>
+#include <iostream>
+#include <vector>
+
+class Transformable {
+public:
+    virtual void rotate(GLdouble dx, GLdouble dy, GLdouble dz) = 0;
+};
+
+class Point : public Transformable {
+    GLdouble x, y, z;
+    void rotate(GLdouble dx, GLdouble dy, GLdouble dz) override;
+};
+
+class Triangle : public Transformable {
+    Point a, b, c;
+    void rotate(GLdouble dx, GLdouble dy, GLdouble dz) override;
+};
+
+class Polygon : public Transformable {
+    //Start with static array for now
+    Triangle triList[];
+    void rotate(GLdouble dx, GLdouble dy, GLdouble dz) override;
+};
+
+#endif
